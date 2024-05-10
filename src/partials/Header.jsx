03e15dd334a6ffa94c1d5a5ba0a36bb11bd8c64b
Yourui <yourui.ruan@gmail.com>
2024-05-10 08:28:46 -0400

    Deleted Notifcations Header and extra sidebar buttons

--- src/partials/Header.jsx
+++ src/partials/Header.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 
 import SearchModal from '../components/ModalSearch';
-import Notifications from '../components/DropdownNotifications';
 import Help from '../components/DropdownHelp';
 import UserMenu from '../components/DropdownProfile';
 import ThemeToggle from '../components/ThemeToggle';
@@ -61,7 +60,6 @@ function Header({ sidebarOpen, setSidebarOpen }) {
               </button>
               <SearchModal id="search-modal" searchId="search" modalOpen={searchModalOpen} setModalOpen={setSearchModalOpen} />
             </div>
-            <Notifications align="right" />
             <Help align="right" />
             <ThemeToggle />
             {/*  Divider */}
